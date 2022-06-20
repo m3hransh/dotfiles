300bf192265ff2ef2cdb186d69edf165d31d344f
Mehran Shahidi <m.mehranshahidi@gmail.com>
2022-06-20 15:02:35 +0430

    ✨ Add Neovim

---
+++ dot_config/nvim/lua/user/lsp/handlers.lua
@@ -0,0 +1,112 @@
+local M = {}
+
+M.setup = function()
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
+
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
+
+  local config = {
+    -- disable virtual text
+    virtual_text = true,
+    -- show signs
+    signs = {
+      active = signs,
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
+
+  vim.diagnostic.config(config)
+
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+  })
+
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
+  })
+end
+
+local function lsp_highlight_document(client)
+  -- Set autocommands conditional on server_capabilities
+  if client.server_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
+  end
+end
+
+local map = vim.api.nvim_buf_set_keymap
+local function map_cond(cap, b, m, key, cmd, opts)
+  if cap then
+    map(b, m, key, cmd, opts)
+  end
+end
+
+local function lsp_keymaps(client, bufnr)
+  local opts = { noremap = true, silent = true }
+  local rc = client.server_capabilities
+
+  map_cond(rc.declaration, bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  map_cond(rc.goto_definition, bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  map_cond(rc.implementation, bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  map_cond(rc.type_definition, bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  map_cond(rc.rename, bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  map_cond(rc.find_references, bufnr, "n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  map_cond(rc.hover, bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  map_cond(rc.signature_help, bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
+  map(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  map(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  map(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
+  map(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float({border ="rounded" })<CR>', opts)
+  map(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  map(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+  map(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  -- you can use <leader>lf for formatting
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+M.on_attach = function(client, bufnr)
+  if client.name == "tsserver" then
+    -- Disable the tsserver formating for null-ls
+    client.server_capabilities.document_formatting = false
+  end
+  lsp_keymaps(client, bufnr)
+  lsp_highlight_document(client)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok then
+  return
+end
+
+M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+return M
